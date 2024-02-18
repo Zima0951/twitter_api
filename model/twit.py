84647aa09de1383078860bac943327b5058fabe8
Zima0951 <159266283+Zima0951@users.noreply.github.com>
2024-02-18 13:41:15 +0300

    Update twit.py

--- model/twit.py
+++ model/twit.py
@@ -6,5 +6,8 @@ class Twit:
     def __init__ (self ,body : str, author : User):
         self.body = body
         self.author = author
+
+    def to_json(self):
+        return {'body':self.body, 'author':self.author}
         
         
